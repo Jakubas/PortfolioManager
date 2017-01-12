c75fef0f734b36ed4e1d5a1ce3468dce0cead031
Daniel Jakubas <dxj452@cs.bham.ac.uk>
2017-01-12 17:03:03 +0000

    Add service layer for stock domain
    Currently the service layer provides base functionality such as adding,
    deleting, retrieving, and updating rows in the database

---
+++ finance-app/src/main/java/my/app/service/StockService.java
@@ -0,0 +1,16 @@
+package my.app.service;
+
+import java.util.List;
+
+import my.app.domain.Stock;
+
+public interface StockService {
+	
+	void saveStock(Stock stock);
+	Stock getStockById(int id);
+	Stock getStockByName(String name);
+	Stock getStockByTicker(String ticker);
+	List<Stock> getStocks();
+	void updateStock(Stock stock);
+	void deleteStock(Stock stock);
+}
