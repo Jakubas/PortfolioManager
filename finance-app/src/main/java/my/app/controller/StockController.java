9f75c8d57f38611751928e6c94d7306f165ea7e9
Daniel Jakubas <dxj452@cs.bham.ac.uk>
2017-01-16 17:57:37 +0000

    Add a controller for the application
    At this stage the controller has bare minimum functionality and is used for
    manual testing purposes.

---
+++ finance-app/src/main/java/my/app/controller/StockController.java
@@ -0,0 +1,30 @@
+package my.app.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import my.app.service.StockInformationService;
+import my.app.service.StockService;
+import my.app.update_database.UpdateStockInformation;
+
+@RestController
+public class StockController {
+
+	private final StockInformationService stockInformationService;
+	private final StockService stockService;
+	
+	@Autowired
+	public StockController(StockInformationService stockInformationService, StockService stockService) {
+		this.stockInformationService = stockInformationService;
+		this.stockService = stockService;
+	}
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String getBaseStockInformation() {
+		UpdateStockInformation update = new UpdateStockInformation(stockInformationService, stockService);
+		update.updateStockInformation("/home/daniel/fyp/data/");
+		return "Downloaded";
+	}
+}
