4ff5c446b1f5656bf6cfc0aafb25ecd9803e844c
Daniel Jakubas <dxj452@cs.bham.ac.uk>
2017-01-20 12:51:50 +0000

    Add DAO layer for StockCalculatedData domain

---
+++ finance-app/src/main/java/my/app/dao/StockCalculatedDataDAOImpl.java
@@ -0,0 +1,51 @@
+package my.app.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import my.app.domain.Stock;
+import my.app.domain.StockCalculatedData;
+
+@Repository
+public class StockCalculatedDataDAOImpl implements StockCalculatedDataDAO {
+
+	private final SessionFactory sessionFactory;
+	
+	@Autowired
+	public StockCalculatedDataDAOImpl(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+	
+	public void saveStockCalculatedData(StockCalculatedData stockCalculatedData) {
+		Session session = sessionFactory.getCurrentSession();
+		session.persist(stockCalculatedData);
+	}
+
+	public StockCalculatedData getStockCalculatedDataById(int id) {
+		Session session = sessionFactory.getCurrentSession();
+		StockCalculatedData stockCalculatedData = session.get(StockCalculatedData.class, id);
+		return stockCalculatedData;
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<StockCalculatedData> getStockCalculatedDatas() {
+		Session session = sessionFactory.getCurrentSession();
+		List<StockCalculatedData> stockCalculatedDatas = session.createCriteria(StockCalculatedData.class).list();
+		return stockCalculatedDatas;
+	}
+
+	public void updateStockCalculatedData(StockCalculatedData stockCalculatedData) {
+		Session session = sessionFactory.getCurrentSession();
+		session.update(stockCalculatedData);
+	}
+
+	public void deleteStockCalculatedData(StockCalculatedData stockCalculatedData) {
+		Session session = sessionFactory.getCurrentSession();
+		session.delete(stockCalculatedData);
+	}
+
+}
