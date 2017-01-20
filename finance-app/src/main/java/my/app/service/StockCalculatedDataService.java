4718a8b5008dd2be63e1fe35239b57e6b8c95c76
Daniel Jakubas <dxj452@cs.bham.ac.uk>
2017-01-20 12:52:14 +0000

    Add service layer for StockCalculatedData domain

---
+++ finance-app/src/main/java/my/app/service/StockCalculatedDataService.java
@@ -0,0 +1,15 @@
+package my.app.service;
+
+import java.util.List;
+
+import my.app.domain.Stock;
+import my.app.domain.StockCalculatedData;
+
+public interface StockCalculatedDataService {
+
+	void saveStockCalculatedData(StockCalculatedData stockCalculatedData);
+	StockCalculatedData getStockCalculatedDataById(int id);
+	List<StockCalculatedData> getStockCalculatedDatas();
+	void updateStockCalculatedData(StockCalculatedData stockCalculatedData);
+	void deleteStockCalculatedData(StockCalculatedData stockCalculatedData);
+}
---
+++ finance-app/src/main/java/my/app/service/StockCalculatedDataServiceImpl.java
@@ -0,0 +1,38 @@
+package my.app.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import my.app.dao.StockCalculatedDataDAOImpl;
+import my.app.domain.StockCalculatedData;
+
+@Service
+public class StockCalculatedDataServiceImpl implements StockCalculatedDataService {
+
+	private final StockCalculatedDataDAOImpl dao;
+	
+	public StockCalculatedDataServiceImpl(StockCalculatedDataDAOImpl dao) {
+		this.dao = dao;
+	}
+	
+	public void saveStockCalculatedData(StockCalculatedData stockCalculatedData) {
+		dao.saveStockCalculatedData(stockCalculatedData);
+	}
+
+	public StockCalculatedData getStockCalculatedDataById(int id) {
+		return dao.getStockCalculatedDataById(id);
+	}
+
+	public List<StockCalculatedData> getStockCalculatedDatas() {
+		return dao.getStockCalculatedDatas();
+	}
+
+	public void updateStockCalculatedData(StockCalculatedData stockCalculatedData) {
+		dao.updateStockCalculatedData(stockCalculatedData);
+	}
+
+	public void deleteStockCalculatedData(StockCalculatedData stockCalculatedData) {
+		dao.deleteStockCalculatedData(stockCalculatedData);
+	}
+}
