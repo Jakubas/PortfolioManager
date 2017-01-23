839b2e235469036e89b4dd5a661508f8d939e242
Daniel Jakubas <dxj452@cs.bham.ac.uk>
2017-01-23 15:47:43 +0000

    Add controller for stockCalculatedData

---
+++ finance-app/src/main/java/my/app/controller/StockCalculatedDataController.java
@@ -0,0 +1,37 @@
+package my.app.controller;
+
+import java.util.Map;
+
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import my.app.domain.StockCalculatedData;
+import my.app.service.StockCalculatedDataService;
+
+@RestController
+public class StockCalculatedDataController {
+	
+	private final StockCalculatedDataService stockCalculatedDataService;
+	
+	@Autowired
+	public StockCalculatedDataController(StockCalculatedDataService stockCalculatedDataService) {
+		this.stockCalculatedDataService = stockCalculatedDataService;
+	}
+	
+	@RequestMapping(value = "/stocks/{stock_id}/calculatedData", method = RequestMethod.GET, produces = "application/json")
+	public String getStockCalculatedData(@PathVariable("stock_id") int stockId) {
+		StockCalculatedData stockCalculatedData = stockCalculatedDataService.getStockCalculatedDataById(stockId);
+	    JSONObject json = new JSONObject();
+	    json.put("stockId", stockCalculatedData.getStockId());
+	    json.put("quarterlyAnnualisedReturn", stockCalculatedData.getQuarterlyAnnualisedReturn());
+	    json.put("oneYearAnnualisedReturn", stockCalculatedData.getOneYearAnnualisedReturn());
+	    json.put("fiveYearAnnualisedReturn", stockCalculatedData.getFiveYearAnnualisedReturn());
+	    json.put("tenYearAnnualisedReturn", stockCalculatedData.getTenYearAnnualisedReturn());
+	    json.put("variance", stockCalculatedData.getVariance());
+	    return json.toString();
+	}
+}
