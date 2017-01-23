234ad7435d9edd81363502a115ac310161c23d32
Daniel Jakubas <dxj452@cs.bham.ac.uk>
2017-01-23 16:17:34 +0000

    Add DAO layer for the user domain

---
+++ finance-app/src/main/java/my/app/dao/UserDAO.java
@@ -0,0 +1,14 @@
+package my.app.dao;
+
+import java.util.List;
+
+import my.app.domain.User;
+
+public interface UserDAO {
+
+	void saveUser(User user);
+	User getUserById(int id);
+	List<User> getUsers();
+	void updateUser(User user);
+	void deleteUser(User user);
+}
---
+++ finance-app/src/main/java/my/app/dao/UserDAOImpl.java
@@ -0,0 +1,51 @@
+package my.app.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import my.app.domain.User;
+
+public class UserDAOImpl implements UserDAO {
+
+	private final SessionFactory sessionFactory;
+	
+	@Autowired
+	public UserDAOImpl(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+	
+	public void saveUser(User user) {
+		Session session = sessionFactory.getCurrentSession();
+		session.persist(user);
+	}
+
+	public User getUserById(int id) {
+		Session session = sessionFactory.getCurrentSession();
+		User user = session.get(User.class, id);
+		return user;
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<User> getUsers() {
+		Session session = sessionFactory.getCurrentSession();	
+		List<User> users = session.createCriteria(User.class).list();
+		return users;
+	}
+
+	public void updateUser(User user) {
+		Session session = sessionFactory.getCurrentSession();
+		session.update(user);
+	}
+
+	public void deleteUser(User user) {
+		Session session = sessionFactory.getCurrentSession();
+		int id = user.getId();
+		user = session.get(User.class, id);
+		if (user != null) {
+			session.delete(user);
+		}
+	}
+}
