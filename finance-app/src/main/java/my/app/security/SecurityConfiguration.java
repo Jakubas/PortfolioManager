aeb8a1de9efc055a74692ffb636cb42624bd1772
Daniel Jakubas <dxj452@cs.bham.ac.uk>
2017-01-25 19:52:08 +0000

    Add user authentication
    After hours of debugging I found that using my custom hashing implementation
    would require alot of custom configurations so I decided to switch to using the
    BCrypt suite and use it for hashing user passwords since it is easy to setup
    and use with the Spring framework.

--- finance-app/src/main/java/my/app/security/SecurityConfiguration.java
+++ finance-app/src/main/java/my/app/security/SecurityConfiguration.java
@@ -1,12 +1,18 @@
 package my.app.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.authentication.dao.ReflectionSaltSource;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -14,17 +20,35 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
-	private MyUserDetailsService userDetailsService;
+	private UserDetailsServiceImpl userDetailsService;
 
+    @Autowired
+    public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("admin").password("admin").roles("ADMIN");
+        auth.inMemoryAuthentication().withUser("test").password("test").roles("USER");
+    }
+    
+    @Bean
+    public DaoAuthenticationProvider authProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(new BCryptPasswordEncoder());
+        return authProvider;
+    }
+    
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService);
+		auth.authenticationProvider(authProvider());
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().anyRequest().fullyAuthenticated();
+		//this will require all endpoints to be authenticated
+//		http.authorizeRequests().anyRequest().fullyAuthenticated();
 		http.httpBasic();
 		http.csrf().disable();
+        http.authorizeRequests()
+        .antMatchers("/users").hasRole("ADMIN");
 	}
 }
