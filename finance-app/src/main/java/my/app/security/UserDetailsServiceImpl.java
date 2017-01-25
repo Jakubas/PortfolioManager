124df1d2e2d61640d414c001493167d5c3ac6719
Daniel Jakubas <dxj452@cs.bham.ac.uk>
2017-01-25 19:51:45 +0000

    Rename file

--- finance-app/src/main/java/my/app/security/UserDetailsServiceImpl.java
+++ finance-app/src/main/java/my/app/security/UserDetailsServiceImpl.java
@@ -5,32 +5,37 @@ import java.util.Collection;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import my.app.service.UserService;
 
 @Service
-public class MyUserDetailsService implements UserDetailsService {
+public class UserDetailsServiceImpl implements UserDetailsService {
 
 	private final UserService userService;
 	
 	@Autowired
-	public MyUserDetailsService(UserService userService) {
+	public UserDetailsServiceImpl(UserService userService) {
 		this.userService = userService;
 	}
 	
 	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+		System.out.println("------------------SGSG---------------------------");
 		my.app.domain.User user = userService.getUserByUserName(userName);
 		if (user == null) {
 			throw new UsernameNotFoundException("Username " + userName + " not found");
 		}
-		return new User(userName, user.getPasswordHash(), getGrantedAuthorities(user));
+		System.out.println(user.getUserName());
+		return new User(user.getUserName(), user.getPasswordHash(), getGrantedAuthorities(user));
 	}
 	
 	public List<GrantedAuthority> getGrantedAuthorities(my.app.domain.User user) {
