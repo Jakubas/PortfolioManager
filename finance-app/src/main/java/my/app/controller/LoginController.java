17f4131632d25cdfc7a83a1d75dc81fe893a2366
Daniel Jakubas <dxj452@cs.bham.ac.uk>
2017-01-30 14:46:05 +0000

    Add controller for login page

---
+++ finance-app/src/main/java/my/app/controller/LoginController.java
@@ -0,0 +1,26 @@
+package my.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class LoginController {
+
+	@RequestMapping(value="/login", method=RequestMethod.GET)
+	public String login() {
+		return "login";
+	}
+	
+	@RequestMapping(value="/login-error", method=RequestMethod.GET)
+	public String loginError(Model model) {
+		model.addAttribute("loginError", true);
+		return "login";
+	}
+	
+	@RequestMapping(value="/logout", method=RequestMethod.POST)
+	public String logout() {
+		return "/";
+	}
+}
