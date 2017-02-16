94b9b726f3797c88f623b4400626a822d2cb7171
Daniel Jakubas <dxj452@cs.bham.ac.uk>
2017-02-16 18:36:25 +0000

    Fix grammar mistakes in goal templates

--- finance-app/src/main/java/my/app/domain/goal/GoalTemplates.java
+++ finance-app/src/main/java/my/app/domain/goal/GoalTemplates.java
@@ -10,13 +10,13 @@ public class GoalTemplates {
 
 	private GoalTemplates() {}
 	
-	private final static String A = "I want _x_% of my stock in _y_ sector";
-	private final static String B = "I want to move _x_% from _y_ sector to _z_ sector";
+	private final static String A = "I want _x_% of my stock in the _y_ sector";
+	private final static String B = "I want to move _x_% of my holdings in _y_ to _z_";
 	private final static String C = "I want to retire in _x_ years";
 	private final static String D = "I want to grow my investments to $_x_";
 	private final static String E = "I want to invest for _x_   _y_";
 	private final static String F = "I want to have a _x_ risk portfolio";
-	private final static String G = "I want to deposit $_x_ per month and grow my investments to $_y_";
+	private final static String G = "I want to deposit $_x_ each month and grow my investments to $_y_";
 	
 	@SuppressWarnings("serial")
 	public final static List<String> GOAL_TEMPLATES = new ArrayList<String>() {{
