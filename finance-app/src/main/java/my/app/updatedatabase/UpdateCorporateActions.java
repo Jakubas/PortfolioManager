42db62ca2e55c4aed666c97b710e695c93866806
Daniel Jakubas <dxj452@cs.bham.ac.uk>
2017-02-27 19:59:13 +0000

    Fix updating stock splits
    I made updating stock splits and dividends more efficient and I also fixed
    some issues that were occuring.

--- finance-app/src/main/java/my/app/updatedatabase/UpdateCorporateActions.java
+++ finance-app/src/main/java/my/app/updatedatabase/UpdateCorporateActions.java
@@ -81,20 +81,49 @@ public class UpdateCorporateActions {
 	
 	private void updateStockSplits(Stock stock, String filePath) {
 		List<StockSplit> stockSplits = CorporateActionParser.parseCSVToStockSplits(stock, filePath);
+		
+		stockSplits.sort(Comparator.comparing(StockSplit::getDate).reversed());
+		List<StockSplit> stockSplitsInDatabase = stock.getStockSplits();
+		double splitRatioToDateReversed = 1;
+		for (StockSplit stockSplit : stockSplits) {
+			splitRatioToDateReversed *= stockSplit.getSplitRatio();
+			stockSplit.setSplitRatioToDateReversed(splitRatioToDateReversed);
+		}
+		
 		stockSplits.sort(Comparator.comparing(StockSplit::getDate));
-		List<StockSplit> stockSplitsInDatabase = stockSplitService.getStockSplits();
 		double splitRatioToDate = 1;
 		for (StockSplit stockSplit : stockSplits) {
-			LocalDate date = stockSplit.getDate();
-			String split = stockSplit.getSplit();
 			splitRatioToDate *= stockSplit.getSplitRatio();
-			if (stockSplitsInDatabase.stream().anyMatch(o -> 
-			o.getStock().getId() == stock.getId() && o.getDate().equals(date) && o.getSplit().equals(split))) {
-				continue;
+			stockSplit.setSplitRatioToDate(splitRatioToDate);
+			
+			StockSplit stockSplitInDatabase;
+			if ((stockSplitInDatabase = getMostSimiliar(stockSplit, stockSplitsInDatabase)) != null) {
+				if (stockSplitInDatabase.getSplitRatioToDate() == stockSplit.getSplitRatioToDate() &&
+						stockSplitInDatabase.getSplitRatioToDateReversed() == stockSplit.getSplitRatioToDateReversed()) {
+					continue;
+				} else {
+					stockSplitInDatabase.setSplitRatioToDate(stockSplit.getSplitRatioToDate());
+					stockSplitInDatabase.setSplitRatioToDateReversed(stockSplit.getSplitRatioToDateReversed());
+					stockSplitService.updateStockSplit(stockSplitInDatabase);
+				}
 			} else {
-				stockSplit.setSplitRatioToDate(splitRatioToDate);
 				stockSplitService.saveStockSplit(stockSplit);
 			}
 		}
+	}
+	
+	private StockSplit getMostSimiliar(StockSplit stockSplit, List<StockSplit> stockSplits) {
+		int stockId = stockSplit.getStock().getId();
+		LocalDate date = stockSplit.getDate();
+		String split = stockSplit.getSplit();
+		for (StockSplit stockSplit2 : stockSplits) {	
+			int stockId2 = stockSplit2.getStock().getId();
+			LocalDate date2 = stockSplit2.getDate();
+			String split2 = stockSplit2.getSplit();
+			if (stockId == stockId2 && date.equals(date2) && split.equals(split2)) {
+				return stockSplit2;
+			}
+		}
+		return null;
 	}
 }
