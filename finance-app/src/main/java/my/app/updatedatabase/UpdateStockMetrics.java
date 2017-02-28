47714149ffd9ffcd260f4e1ba1a5de6a0560507a
Daniel Jakubas <dxj452@cs.bham.ac.uk>
2017-02-28 19:55:05 +0000

    Add print statements

--- finance-app/src/main/java/my/app/updatedatabase/UpdateStockMetrics.java
+++ finance-app/src/main/java/my/app/updatedatabase/UpdateStockMetrics.java
@@ -19,16 +19,21 @@ public class UpdateStockMetrics {
 		this.stockService = stockService;
 	}
 	
-	public void updateStockCalculations() {
+	public void updateStockMetrics() {
 		List<Stock> stocks = stockService.getStocks();
+		int i = 1;
 		for (Stock stock : stocks) {
-			System.out.println(stock.getId());
 			Double quarterlyAnnualisedReturn = StockDataCalculations.calculateQuarterlyAnnualisedReturn(stock);
 			Double oneYearAnnualisedReturn = StockDataCalculations.calculate1YrAnnualisedReturn(stock);
 			Double fiveYearAnnualisedReturn = StockDataCalculations.calculate5YrAnnualisedReturn(stock);
 			Double tenYearAnnualisedReturn = StockDataCalculations.calculate10YrAnnualisedReturn(stock);
 			Double variance = Risk.calculateVariance(stock);
-			
+//			System.out.println(stock.getId());
+//			System.out.println(quarterlyAnnualisedReturn);
+//			System.out.println(oneYearAnnualisedReturn);
+//			System.out.println(fiveYearAnnualisedReturn);
+//			System.out.println(tenYearAnnualisedReturn);
+//			System.out.println(variance);
 			StockMetrics stockMetrics = 
 					new StockMetrics(stock, quarterlyAnnualisedReturn, oneYearAnnualisedReturn, 
 							fiveYearAnnualisedReturn, tenYearAnnualisedReturn, variance);
@@ -36,12 +41,15 @@ public class UpdateStockMetrics {
 			StockMetrics prevStockMetrics = 
 					stockMetricsService.getStockMetricsById(stock.getId());
 			if (prevStockMetrics != null &&
-				!stockMetrics.getThreeMonthAnnualisedReturn().equals(prevStockMetrics.getThreeMonthAnnualisedReturn())) {
+					!stockMetrics.getThreeMonthAnnualisedReturn().equals(prevStockMetrics.getThreeMonthAnnualisedReturn())) {
 				stockMetricsService.deleteStockMetrics(prevStockMetrics);
+				System.out.println("updating");
 				stockMetricsService.saveStockMetrics(stockMetrics);
 			} else if(prevStockMetrics == null) {
 				stockMetricsService.saveStockMetrics(stockMetrics);
+				System.out.println("saved");
 			}
+			System.out.println(i++ + " / " + stocks.size() + " stock metrics updated");
 		}
 	}
 }
