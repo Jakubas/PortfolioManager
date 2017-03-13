985beb2e737be73bfe4a7f24c96af4e9068d38e8
Daniel Jakubas <dxj452@cs.bham.ac.uk>
2017-03-13 19:04:56 +0000

    Add price chart to stock information view

--- finance-app/src/main/java/my/app/controllers/StockController.java
+++ finance-app/src/main/java/my/app/controllers/StockController.java
@@ -1,5 +1,7 @@
 package my.app.controllers;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import my.app.domains.stock.Stock;
+import my.app.domains.stock.StockDailyInformation;
 import my.app.services.stock.StockService;
 
 @Controller
@@ -30,10 +33,32 @@ public class StockController {
 	}
 	
 	@RequestMapping(value = "stocks/{id}", method=RequestMethod.GET)
-	public String getStockGraph(@PathVariable("id") int id, Model model) {
+	public String getStockInformation(@PathVariable("id") int id, Model model) {
 		Stock stock = stockService.getStockById(id);
+		List<StockDailyInformation> sdis = stock.getStockDailyInformations();
+		sdis.sort(Comparator.comparing(StockDailyInformation::getDate));
+		List<String> dates = getDates(sdis);
+		List<Double> prices = getPrices(sdis);
 		model.addAttribute("stock", stock);
+		model.addAttribute("dates", dates);
+		model.addAttribute("prices", prices);
 		model.addAttribute("stockMetrics", stock.getStockMetrics());
 		return "stockInformation";
+	}
+	
+	private List<String> getDates(List<StockDailyInformation> sdis) {
+		List<String> dates = new ArrayList<String>();
+		for (StockDailyInformation sdi : sdis) {
+			dates.add(sdi.getDate().toString());
+		}
+		return dates;
+	}
+	
+	private List<Double> getPrices(List<StockDailyInformation> sdis) {
+		List<Double> prices = new ArrayList<Double>();
+		for (StockDailyInformation sdi : sdis) {
+			prices.add(sdi.getAdjCloseStockSplits());
+		}
+		return prices;
 	}
 }
