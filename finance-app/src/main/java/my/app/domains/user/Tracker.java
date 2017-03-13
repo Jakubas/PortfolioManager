75f21ed9c85965adb6cd30a6995bc7ed3779f4c2
Daniel Jakubas <dxj452@cs.bham.ac.uk>
2017-03-13 14:39:21 +0000

    Add domain for stock tracking

---
+++ finance-app/src/main/java/my/app/domains/user/Tracker.java
@@ -0,0 +1,47 @@
+package my.app.domains.user;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+
+import my.app.domains.stock.Stock;
+
+@Entity
+public class Tracker {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int id;
+	
+	@NotNull
+	@ManyToOne
+	@JoinColumn(name = "stock_id")
+	private Stock stock;
+	
+	@NotNull
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+	
+	public Tracker(Stock stock, User user) {
+		
+	}
+	
+	public Tracker() {}
+
+	public int getId() {
+		return id;
+	}
+
+	public Stock getStock() {
+		return stock;
+	}
+
+	public User getUser() {
+		return user;
+	}
+}
