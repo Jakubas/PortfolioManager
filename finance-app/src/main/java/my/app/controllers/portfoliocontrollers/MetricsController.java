094162e4efa510219daa34b39f9fa59aa9e8f748
Daniel Jakubas <dxj452@cs.bham.ac.uk>
2017-03-17 16:32:53 +0000

    Add S&P 500 index values to controller model
    The S&P 500 index values are used as a performance metric to compare the
    performance of the user's portfolio to the index. The values account for cash
    inflows and outflows (stock sells/buys).

--- finance-app/src/main/java/my/app/controllers/portfoliocontrollers/MetricsController.java
+++ finance-app/src/main/java/my/app/controllers/portfoliocontrollers/MetricsController.java
@@ -1,6 +1,7 @@
 package my.app.controllers.portfoliocontrollers;
 
 import java.security.Principal;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -14,11 +15,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import my.app.cashflow.HoldingTransaction;
 import my.app.domains.portfolio.PortfolioDailyInformation;
 import my.app.domains.portfolio.StockInPortfolio;
+import my.app.domains.stock.IndexDailyInformation;
 import my.app.domains.user.User;
+import my.app.services.portfolio.PortfolioDailyInformationService;
 import my.app.services.portfolio.PortfolioService;
 import my.app.services.portfolio.StockInPortfolioService;
+import my.app.services.stock.IndexDailyInformationService;
 import my.app.services.stock.StockService;
 import my.app.services.user.UserService;
+import my.app.updatedatabase.UpdatePortfolioDailyInformation;
+import my.app.utilities.DateUtility;
 
 //Portfolio metrics
 @Controller
@@ -28,14 +34,20 @@ public class MetricsController {
 	private final UserService userService;
 	private final StockService stockService;
 	private final PortfolioService portfolioService;
+	private final IndexDailyInformationService indexDailyInformationService;
+	private final PortfolioDailyInformationService portfolioDailyInformationService;
 	
 	@Autowired
 	public MetricsController(StockInPortfolioService stockInPortfolioService, 
-			UserService userService, StockService stockService, PortfolioService portfolioService) {
+			UserService userService, StockService stockService, PortfolioService portfolioService,
+			IndexDailyInformationService indexDailyInformationService, 
+			PortfolioDailyInformationService portfolioDailyInformationService) {
 		this.stockInPortfolioService = stockInPortfolioService;
 		this.userService = userService;
 		this.stockService = stockService;
 		this.portfolioService = portfolioService;
+		this.indexDailyInformationService = indexDailyInformationService;
+		this.portfolioDailyInformationService = portfolioDailyInformationService;
 	}
 	
 	@RequestMapping(value = "portfolio/metrics", method = RequestMethod.GET)
@@ -47,11 +59,15 @@ public class MetricsController {
 		for (int i = 0; i < sectors.size(); i++) {
 			sectorValues[i] = user.sectorValue(sectors.get(i));
 		}
+		List<HoldingTransaction> transactions = getTransactions(user);
 		model.addAttribute("sectors", sectors);
 		model.addAttribute("sectorValues", sectorValues);
 		model.addAttribute("dayValues", getDayValues(user));
 		model.addAttribute("dates", getDates(user));
-		model.addAttribute("transactions", getTransactions(user));
+		model.addAttribute("transactions", transactions);
+		model.addAttribute("indexValues", getIndexValues(user, transactions));
+		UpdatePortfolioDailyInformation updi = new UpdatePortfolioDailyInformation(portfolioDailyInformationService, userService);
+		updi.updatePortfolioDailyInformationFor(user);
 		return "portfolio/metrics";
 	}
 
@@ -77,6 +93,73 @@ public class MetricsController {
 			dates[i] = value;
 		}
 		return dates;
+	}
+	
+	private Object getIndexValues(User user, List<HoldingTransaction> transactions) {
+		List<IndexDailyInformation> idis = indexDailyInformationService.getIndexDailyInformations();
+		List<PortfolioDailyInformation> pdis = user.getPortfolioDailyInformations();
+		pdis.sort(Comparator.comparing(PortfolioDailyInformation::getDate));
+		idis.sort(Comparator.comparing(IndexDailyInformation::getDate));
+		if (pdis == null || pdis.isEmpty()) {
+			return null;
+		}
+		LocalDate earliestDate = pdis.get(0).getDate();
+		LocalDate latestDate = pdis.get(pdis.size()-1).getDate();
+		idis.removeIf(o -> o.getDate().isBefore(earliestDate));
+		idis.removeIf(o -> o.getDate().isAfter(latestDate));
+		double[] indexValues = new double[pdis.size()];
+		transactions.sort(Comparator.comparing(HoldingTransaction::getDate));
+		
+		//fill the gaps inbetween dates;
+		List<IndexDailyInformation> idisToAdd = new ArrayList<IndexDailyInformation>();
+		for (int i = 0; i < idis.size()-1; i++) {
+			IndexDailyInformation idi = idis.get(i);
+			IndexDailyInformation idi2 = idis.get(i+1);
+			LocalDate date = idi.getDate();
+			LocalDate date2 = idi2.getDate();
+			while (DateUtility.daysBetween(date, date2) > 1) {
+				IndexDailyInformation idiToAdd = new IndexDailyInformation();
+				idiToAdd.setValuesFrom(idi);
+				date = date.plusDays(1);
+				idiToAdd.setDate(date);
+				idisToAdd.add(idiToAdd);
+			}
+		}
+		while (idis.size() + idisToAdd.size() < pdis.size()) {
+			IndexDailyInformation idi = idis.get(idis.size()-1);
+			LocalDate date = idi.getDate();
+			IndexDailyInformation idiToAdd = new IndexDailyInformation();
+			idiToAdd.setValuesFrom(idi);
+			date = date.plusDays(1);
+			idiToAdd.setDate(date);
+			idisToAdd.add(idiToAdd);
+		}
+		idis.addAll(idisToAdd);
+		idis.sort(Comparator.comparing(IndexDailyInformation::getDate));
+		double shareAmount = 0;
+		int j = 0;
+		for (int i = 0; i < indexValues.length; i++) {
+			IndexDailyInformation idi = idis.get(i);
+			if (j < transactions.size()) {
+				HoldingTransaction transaction = transactions.get(j);
+				while (j < transactions.size() && 
+						(transaction.getDate().isBefore(idi.getDate()) || 
+						 transaction.getDate().isEqual(idi.getDate()))) {
+					if (transaction.isBuy()) {
+						shareAmount += (transaction.getValue() / idi.getClose());
+					} else {
+						shareAmount -= (transaction.getValue() / idi.getClose());
+					}
+					j++;
+					if (j >= transactions.size()) {
+						break;
+					}
+					transaction = transactions.get(j);
+				}
+			}
+			indexValues[i] = shareAmount * idi.getClose();
+		} 
+		return indexValues;
 	}
 	
 	private List<HoldingTransaction> getTransactions(User user) {
